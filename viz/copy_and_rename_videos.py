a5805f80d38e688b1b4c195984aac6eec917e722
Kaustubh Sridhar <kaysri2007@gmail.com>
2025-04-14 15:46:13 -0400

    updated viz info

---
+++ viz/copy_and_rename_videos.py
@@ -0,0 +1,10 @@
+import os 
+from viz_info import video_paths
+
+new_fol = 'videos_copy_simplified_dont_delete'
+os.makedirs(new_fol, exist_ok=True)
+
+for key_of_method_task, location in video_paths.items():
+    key_of_task_method = f'{key_of_method_task.split("_")[-1]}_{"_".join(key_of_method_task.split("_")[:-1])}'
+    print(f'{key_of_method_task} -> {key_of_task_method}')
+    os.system(f'cp {location} {new_fol}/{key_of_task_method}.mp4')
